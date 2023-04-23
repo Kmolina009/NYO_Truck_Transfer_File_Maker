c1d7085d2e2d4b46aa50ebc51dd41fda6c449b17
Keven Molina <kevenmolina@gmail.com>
2023-04-23 19:31:31 -0400

    adjusted project structure for modularity

--- Scripts/packed_shipment_filter.py
+++ Scripts/packed_shipment_filter.py
@@ -5,8 +5,7 @@ Created on Sun Apr  2 15:11:17 2023
 """
 import csv
 
-# with open(r"C:\Users\Keven\OneDrive\Desktop\NYO_Stuff\Dummy_Data\Test_Copy\NY_Truck_Transfers_NYO_Dummy_Data_CSV.csv","r") as csv_file:
-with open(r"Dummy_Data\Test_Copy\NY_Truck_Transfers_NYO_Dummy_Data_CSV.csv","r") as csv_file:
+with open(r".\Dummy_Data\Test_Copy\NY_Truck_Transfers_NYO_Dummy_Data_CSV.csv","r") as csv_file:
     csv_reader = csv.DictReader(csv_file)
 #    NOTE - Variable names need to be cleaned up, refer to style guide
 # Entries to be zipped
@@ -31,8 +30,6 @@ with open(r"Dummy_Data\Test_Copy\NY_Truck_Transfers_NYO_Dummy_Data_CSV.csv","r")
     next(csv_file)
     next(csv_file)
     for row in csv_reader:
-        # Name, Status, Item#, Client#, DID, Description, Sender, Recipient, Venue, Sale#, Sale_Deadline, Value, Item_Count, Location, Bin#, Box_Count,
-        # print( row['Name'], row['Status'], row['Item #'], row['Client #'], row['DID / REF'], row['Description'], row['Sender'], row['Recipient'], row['Venue'], row['Sale #'], row['Sale Deadline'], row['Value'], row['Item Count'], row['Location'], row['Bin #'], row['Box Count'] )
         Names.append( row['Name'])
         Status.append( row['Status']) 
         Item_Numbers.append( row['Item #'])              
@@ -49,8 +46,6 @@ with open(r"Dummy_Data\Test_Copy\NY_Truck_Transfers_NYO_Dummy_Data_CSV.csv","r")
         Locations.append( row['Location'])
         Bin_Numbers.append( row['Bin #'])
         Box_Counts.append( row['Box Count'])
-        # data.append(row['Name'],row['Status'],row['Item_Number'],row['Client'],row['DID'],row'[Description'],row['Sender'],row['Recipient'],row['Venue'],row['Sales_Number'],row['Sale_Deadline'],row['Value'],row['Item_Count'],row'[Location'],row['Bin'],row['Box_Count'])
-        
         shipments = []
         
         for i in range(len(Names)-1):
