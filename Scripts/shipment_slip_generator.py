49b5fb278f90fbb805e20f303526ac839d4806b8
Keven Molina <kevenmolina@gmail.com>
2023-04-23 20:09:44 -0400

    initial commit for this branch, 'pseudo-code'

---
+++ Scripts/shipment_slip_generator.py
@@ -0,0 +1,8 @@
+# END GOAL -> Create the docs neccesary for either a provided directory or have it made,
+
+# Take a provided directory(ask user),
+# Take data from packed shipments,
+# Check the directory for files with names that contain D.IDs
+# If matchs are found, remove them from the prep list(Avoid duplicate entries)
+
+# Once it's been determined which emtris are not in the specified directory, then document creation can start
