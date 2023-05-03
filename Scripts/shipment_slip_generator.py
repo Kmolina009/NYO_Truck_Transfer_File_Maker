8f296e69f08e193f5fde373f7c2177503809d816
Keven Molina <kevenmolina@gmail.com>
2023-05-02 21:59:48 -0400

    in the process creating a function that will gather d.id's from a specified directory then checking if it's in the list of packedshipments

--- Scripts/shipment_slip_generator.py
+++ Scripts/shipment_slip_generator.py
@@ -1,4 +1,5 @@
 import os
+import re
 from packed_shipment_filter import packedshipments
 # import packedshipments fr
 
@@ -12,13 +13,26 @@ from packed_shipment_filter import packedshipments
 # where_the_entries_are =input()
 directory = '../files_to_write_docs_too'  # replace with the path to your directory
 entries = os.scandir(directory)
+starting_patterning = re.compile('^D_ID_')
+
+entries_found = []
 
 for entry in entries:
-    if entry.is_file():
-        print('File:', entry.name)
-    elif entry.is_dir():
-        print('Directory:', entry.name)
-            # do something with the contents of the file
+    if entry.is_file() and starting_patterning.match(entry.name):
+        entries_found.append(entry.name[5:-5])
+print(entries_found)
+
+for item in entries_found:
+    for id in packedshipments:
+        found = False
+        if id.get("DID / REF") == item:
+            print("This entry already exist")
+            found = True
+            break
+        if found:
+            print( "item is present in original_list.")
+        else:
+            print( "item is not present in original_list.")
 
 # Once it's been determined which emtris are not in the specified directory, then document creation can start
 # Have Template
